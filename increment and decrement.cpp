f2dc90e5f6f83cdc55a5e874d8b148b28785f64c
Nitin-M-G <132366804+Nitin-M-G@users.noreply.github.com>
2023-05-17 14:09:27 +0530

    Update increment and decrement.cpp

--- increment and decrement.cpp
+++ increment and decrement.cpp
@@ -10,27 +10,27 @@ int main()
     int a{2};
     int result{0};
 
-    //cout<<endl<<" The increment part is demonstrated here "<<endl;
+    cout<<endl<<" The increment part is demonstrated here "<<endl;
     //Post-increment is demonstrated
-    // cout<<a<<endl;
-    // result=a++;
-    // cout<<result<<endl;    
-    // cout<<a<<endl;
+    cout<<a<<endl;
+    result=a++;
+    cout<<result<<endl;    
+    cout<<a<<endl;
 
 
     // //Pre-increment is demonstrated 
-    // cout<<a<<endl;
-    // result=++a;
-    // cout<<result<<endl;    
-    // cout<<a<<endl;
+    cout<<a<<endl;
+    result=++a;
+    cout<<result<<endl;    
+    cout<<a<<endl;
 
 
      cout<<endl<<"The decrement part is demonstrated here "<<endl;
-    // //Pre-decrement is demonstrated 
-    // cout<<a<<endl;
-    // result=--a;
-    // cout<<result<<endl;    
-    // cout<<a<<endl;
+    //Pre-decrement is demonstrated 
+    cout<<a<<endl;
+    result=--a;
+    cout<<result<<endl;    
+    cout<<a<<endl;
 
     //Post-decrement is demonstrated 
     cout<<a<<endl;
