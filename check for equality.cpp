227ff16fb7739bbe74d02b53cbd5cf34200ff964
Nitin-M-G <132366804+Nitin-M-G@users.noreply.github.com>
2023-05-18 13:16:20 +0530

    check for equality

---
+++ check for equality.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    bool equal_result{false};
+    bool not_equal_result{false};
+
+    int num1{},num2{};
+
+    cout<<" Enter the two numbers "<<endl;
+    cin>>num1>>num2;
+    equal_result=(num1==num2);
+    not_equal_result=(num1!=num2);
+    cout<<" Comparision result(equals):"<<equal_result<<endl;
+    cout<<" Comparision result(not equals):"<<not_equal_result<<endl;
+
+
+    char char1{},char2{};
+
+    cout<<" Enter the two characters "<<endl;
+    cin>>char1>>char2;
+    equal_result=(char1==char2);
+    not_equal_result=(char1!=char2);
+    cout<<" Comparision result (equals) :"<<equal_result<<endl;
+    cout<<" Comparision result (not equals) :"<<not_equal_result<<endl;
+
+
+}
