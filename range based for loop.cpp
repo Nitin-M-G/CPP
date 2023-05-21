096d99bc8b7b3164244362d225f4fc4d4872e992
Nitin-M-G <132366804+Nitin-M-G@users.noreply.github.com>
2023-05-21 14:33:05 +0530

    Create range based for loop.cpp
    Range based for loop demonstration where the limit are set according to the size of the variable ranging from arrays , vectors and characters.

---
+++ range based for loop.cpp
@@ -0,0 +1,51 @@
+//range based for loop
+
+#include<iostream>
+#include<vector>
+#include<iomanip>  //library required for manipulating the output statements
+
+using namespace std;
+
+int main()
+{
+   int scores[]{10,20,30};
+
+   for(auto score:scores)
+   {
+    cout<<score<<endl;
+   }
+
+
+   vector<double>tempreatures{87.9,90.6,60.5};
+   double average_tempreature{};
+   double sum_of_tempreature{0};
+
+   for(auto temp:tempreatures)
+   {
+    sum_of_tempreature+=temp;
+   }
+
+   if(tempreatures.size()!=0)
+   {
+   average_tempreature=sum_of_tempreature/tempreatures.size();
+   }
+   else{
+    cout<<" The vector is empty "<<endl;
+   }
+   cout<<fixed<<setprecision(1);
+   cout<<" The sum of tempreatures is "<<sum_of_tempreature<<endl;
+   cout<<" The average tempreature is "<<average_tempreature<<endl;
+
+
+   for(auto val:{1,2,3,4,5})
+   {
+    cout<<val<<endl;
+   }
+
+   for(auto c:" This is a test ")
+   {
+    cout<<c;
+   }
+
+   
+}
