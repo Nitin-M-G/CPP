10baa7d5cde4ee3310a3c32404cfae461dcec3d2
Nitin-M-G <132366804+Nitin-M-G@users.noreply.github.com>
2023-06-18 19:05:33 +0530

    Create function_parameters.cpp
    incomplete code

---
+++ function_parameters.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void pass_by_value1(int num);
+void pass_by_value2(string s);
+void pass_by_value3(vector<string>v);
+void print_vector(vector<string>v);
+
+int main()
+{
+
+}
